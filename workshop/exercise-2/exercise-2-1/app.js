1dae053848e0ddc772880481bc7d4044e9e0ec59
“Marc <marc_piantone@live.fr>
2020-06-18 09:41:01 -0400

    Answer exercise-2 (sort of)

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -6,6 +6,7 @@
 
 
 //Add css to work with html :
+
 const stylesheet = document.createElement('link')
 stylesheet.rel = 'stylesheet';
 stylesheet.href = 'styles.css';
@@ -13,22 +14,46 @@ document.head.appendChild(stylesheet);
 
 
 // Preset values
+
 const FROGS = 3;
 const track = document.getElementById('track');
 
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
 
 for(i=0;i<FROGS;i++){
+
+    // 2. Create li
     let lane = document.createElement('li');
+
+    // 4. Assign an id to each lane
+    lane.id = `lane ${i+1}`;
+
+    // Append new elements to li (lanes)
     track.appendChild(lane);
+
+    // Select randomly frogs from frogStable.js
+    let random = Math.floor(Math.random()*6);
+    let frog = frogStable[random];
+
+    // 3. Create span (<- THESE ARE THE FROGS) and add it to the li
+    let contester = document.createElement('span');
+    contester.innerHTML = `${frog.name} ${frog.number}`;
+    contester.className = `frog`;
+    lane.appendChild(contester);
+
+    // Push the frogs to an array of racers (?)
+    let racers = [];
+    racers.push(frog);
+
+    // Reset progress of racers
+    racers[lane].progress = 0;
 }
 
-// 2. Create an array that displays the progression of the frog during the race
-
-let progressionBar =[];
 
 
 
-// 3. Create span and add it to the li <- THESE ARE THE FROGS (?)
 
-// 4. Assign an id to each lane
+
+
+
+
