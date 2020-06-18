be7efcddf527d647ec4079593379e4d5d083cf98
“Marc <marc_piantone@live.fr>
2020-06-17 21:48:05 -0400

    Second steps exercise-2

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,10 +1,34 @@
+// Make .js create HTML - Recap : 
+// 1. grab html section
+// 2. create elements in mem
+// 3. add content to elements
+// 4. appendChild html to include new elements
+
+
+//Add css to work with html :
+const stylesheet = document.createElement('link')
+stylesheet.rel = 'stylesheet';
+stylesheet.href = 'styles.css';
+document.head.appendChild(stylesheet);
+
+
 // Preset values
 const FROGS = 3;
+const track = document.getElementById('track');
 
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
 
-// 2. Create li
+for(i=0;i<FROGS;i++){
+    let lane = document.createElement('li');
+    track.appendChild(lane);
+}
 
-// 3. Create span and add it to the li
+// 2. Create an array that displays the progression of the frog during the race
+
+let progressionBar =[];
+
+
+
+// 3. Create span and add it to the li <- THESE ARE THE FROGS (?)
 
 // 4. Assign an id to each lane
