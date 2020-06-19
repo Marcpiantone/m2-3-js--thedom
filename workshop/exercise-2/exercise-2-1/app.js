f91c0ce502f71f41bf7c9d7e65a83d4dd6eeaa53
“Marc <marc_piantone@live.fr>
2020-06-19 15:50:55 -0400

    Answer exercise 2.3

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,60 +1,65 @@
-// Make .js create HTML - Recap : 
+// Make .js create HTML - Recap :
 // 1. grab html section
 // 2. create elements in mem
 // 3. add content to elements
 // 4. appendChild html to include new elements
 
-
 //Add css to work with html :
 
-const stylesheet = document.createElement('link')
-stylesheet.rel = 'stylesheet';
-stylesheet.href = 'styles.css';
+const stylesheet = document.createElement("link");
+stylesheet.rel = "stylesheet";
+stylesheet.href = "styles.css";
 document.head.appendChild(stylesheet);
-
 
 // Preset values
 
 const FROGS = 3;
-const track = document.getElementById('track');
+const track = document.getElementById("track");
+const racers = [];
 
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
 
-for(i=0;i<FROGS;i++){
+for (i = 0; i < FROGS; i++) {
+  //2.1 Create li
+  let lane = document.createElement("li");
 
-    // 2. Create li
-    let lane = document.createElement('li');
+  //2.1 Assign an id to each lane
+  lane.id = `lane ${i + 1}`;
 
-    // 4. Assign an id to each lane
-    lane.id = `lane ${i+1}`;
+  //2.1 Append new elements to li (lanes)
+  track.appendChild(lane);
 
-    // Append new elements to li (lanes)
-    track.appendChild(lane);
+  //2.2 Select randomly frogs from frogStable.js
+  let random = Math.floor(Math.random() * 5);
+  let frog = frogStable[random];
 
-    //Select randomly frogs from frogStable.js
-    let random = Math.floor(Math.random()*5);
-    let frog = frogStable[random];
-
-    //3. Create span (<- THESE ARE THE FROGS) and add it to the li
-    let contester = document.createElement('span');
-    contester.innerHTML = `${frog.name} ${frog.number}`;
-    contester.className = `frog`;
-    contester.style.background = `${frog.color}`;
-    lane.appendChild(contester);
-
-    // // Push the frogs to an array of racers (?)
-    // let racers = [];
-    // racers.push(frog);
-
-    // // Reset progress of racers
-    // racers[lane].progress = 0;
+  //2.1 Create span (<- THESE ARE THE FROGS) and add it to the li
+  let contester = document.createElement("span");
+  contester.innerHTML = `${frog.name} ${frog.number}`;
+  contester.style.background = `${frog.color}`;
+  contester.className = `frog`;
+  contester.progress = 0;
+  contester.jumpStrength = Math.random() * 20 + 5;
+  lane.appendChild(contester);
+  //console.log("Before put into Racers: ", typeof contester.progress);
+  racers.push(contester);
 }
 
+let jumpFunction = function () {
+  racers.forEach(function (racer) {
+    console.log("Racer: ", racer);
+    racer.progress += racer.jumpStrength;
+    racer.style.left = racer.progress + "%";
+    if (racer.progress >= 100) {
+      winnerIs(racer);
+    }
+  });
+};
 
+let winnerIs = function (racer) {
+  console.log(racer.innerHTML + " has won!");
+  racer.style.left = "100%";
+  clearInterval(raceTimer);
+};
 
-
-
-
-
-
-
+let raceTimer = setInterval(jumpFunction, 1000);
